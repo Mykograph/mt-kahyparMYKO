41973c2e11e36b6e6ed754a619e7cbfbcf2d30a5
Mykolas Pabst <mykolaslinus@gmail.com>
2026-06-20 17:45:05 +0200

    Context Value to -10

--- mt-kahypar/partition/context.h
+++ mt-kahypar/partition/context.h
@@ -76,7 +76,7 @@ struct PartitioningParameters {
   std::string fixed_vertex_filename { };
   std::string constraint_filename { };
   std::string constraint_folder = "";
-  HyperedgeWeight constraint_weight { -100 };
+  HyperedgeWeight constraint_weight { -10 };
   std::string graph_partition_output_folder {};
   std::string graph_partition_filename { };
   std::string graph_community_filename { };
